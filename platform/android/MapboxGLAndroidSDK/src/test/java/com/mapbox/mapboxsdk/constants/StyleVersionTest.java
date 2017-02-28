ec3b72ed31c53100496815d079d45d9e26b4e5ce
Guardiola31337 <pablo.guardiola@mapbox.com>
2017-02-28 17:10:58 -0500

    [android] refactor move unit tests from test app to sdk

--- platform/android/MapboxGLAndroidSDK/src/test/java/com/mapbox/mapboxsdk/constants/StyleVersionTest.java
+++ platform/android/MapboxGLAndroidSDK/src/test/java/com/mapbox/mapboxsdk/constants/StyleVersionTest.java
@@ -1,8 +1,6 @@
 package com.mapbox.mapboxsdk.constants;
 
 
-import com.mapbox.mapboxsdk.testapp.model.constants.AppConstant;
-
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
